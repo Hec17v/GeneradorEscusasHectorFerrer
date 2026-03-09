a74b40137472fe1dcda64d2f6bef733eac1960f2
Hec17v <72463386+Hec17v@users.noreply.github.com>
2026-03-09 19:56:24 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+
+ const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+const quien = who[Math.floor(Math.random()*who.length)];
+const accion = action[Math.floor(Math.random()*action.length)];
+const que = what[Math.floor(Math.random()*what.length)];
+const cuando = when[Math.floor(Math.random()*when.length)];
+
+
+document.getElementById("excu").innerHTML = quien + " " + accion + " " +  que + " " + cuando;
   console.log("Hello Rigo from the console!");
 };
